513668e87959e33f648167a2b734ad93b9a6d465
aoleksiag <aoleksiag@gmail.com>
2017-10-17 13:46:18 +0200

    dodatkowy sygnał do testbenche'a

--- HighPass/matlab/notch.m
+++ HighPass/matlab/notch.m
@@ -10,8 +10,8 @@ close all;
 
 format long
 liczba_bit=20;
-n = 2;      %rz¹d filtru
-fs = 48000;    %czêstotliwoœæ próbkowania
+n = 4;      %rz¹d filtru
+fs = 2000;    %czêstotliwoœæ próbkowania
 f = 50;       %czêstotliwoœæ wyciêcia
 f1 = f-3;     %czêstotliwoœæ mniejsza od f dla której ampiltuda spada o 3dB
 f2 = f+3;     %czêstotliwoœæ wiêksza od f dla której ampiltuda spada o 3dB
@@ -24,7 +24,7 @@ ftype = 'stop';             %typ filtru pasmowo zaporowy
 [SOS, G] = tf2sos(b,a);     %wytworzenie wspó³czynników do biquada
 
 
-for i=1:1
+for i=1:n/2
     for j=1:6
         [A,sos2(i,j),sos3(i,j)]=fixpoint(SOS(i,j),liczba_bit);
     end
@@ -32,7 +32,7 @@ end
 
 [l,k,m]=fixpoint(G^(1/2),liczba_bit);
 
-hdl_creator(sos3,1,liczba_bit,m)
+hdl_creator(sos3,n/2,liczba_bit,m)
 
 N=2048;         %iloœæ próbek do wykresu
 ts=1/fs;        %okres próbkowania
--- HighPass/matlab/src/top_filter.v
+++ HighPass/matlab/src/top_filter.v
@@ -17,17 +17,34 @@ filter_sos
 	 #(.COEF_SIZE(COEF_SIZE),
  	   .DATA_SIZE(DATA_SIZE),
  	  .B0(20'd262144),
-	  .B1(20'd720683),
+	  .B1(20'd0),
 	  .B2(20'd262144),
-	  .A1(20'd586389),
-	  .A2(20'd206831),
-	  .GAIN(20'd9058))
+	  .A1(20'd1045107),
+	  .A2(20'd258673),
+	  .GAIN(20'd260403))
 	sos_stage_1 (
  	 .data_in(data_in),
 	 .reset(reset),
 	 .clk(clk),
-	 .data_out(data_out),
+	 .data_out(data_connection_1),
 	 .sample_trig(sample_trig),
 	 .filter_done(filter_done_first)
+	);
+filter_sos
+	 #(.COEF_SIZE(COEF_SIZE),
+ 	   .DATA_SIZE(DATA_SIZE),
+ 	  .B0(20'd262144),
+	  .B1(20'd0),
+	  .B2(20'd262144),
+	  .A1(20'd3471),
+	  .A2(20'd258673),
+	  .GAIN(20'd260403))
+	sos_stage_2 (
+ 	 .data_in(data_connection_1),
+	 .reset(reset),
+	 .clk(clk),
+	 .data_out(data_out),
+	 .sample_trig(filter_done_first),
+	 .filter_done(filter_done_second)
 	);
 endmodule

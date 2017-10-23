734cb392d443b61abc80f5552cec1cdc2602b9e0
Adrian-Komputer\Adrian <aoleksiag@gmail.com>
2017-10-23 12:56:37 +0200

    sygnał ecg

---
+++ HighPass/matlab/top_filter.v
@@ -0,0 +1,50 @@
+`timescale 1ns/1ps
+
+module top_filter
+ 	#(parameter COEF_SIZE=20,
+	  parameter DATA_SIZE=24)
+	(input [DATA_SIZE-1:0] data_in,
+	  input reset,
+	  input clk,
+	  input sample_trig,
+	  output [DATA_SIZE-1:0] data_out
+ 	 );
+
+wire [DATA_SIZE-1:0] data_connection_1;
+wire filter_done_first;
+
+filter_sos
+	 #(.COEF_SIZE(COEF_SIZE),
+ 	   .DATA_SIZE(DATA_SIZE),
+ 	  .B0(20'd262144),
+	  .B1(20'd528759),
+	  .B2(20'd262144),
+	  .A1(20'd532145),
+	  .A2(20'd259127),
+	  .GAIN(20'd260692))
+	sos_stage_1 (
+ 	 .data_in(data_in),
+	 .reset(reset),
+	 .clk(clk),
+	 .data_out(data_connection_1),
+	 .sample_trig(sample_trig),
+	 .filter_done(filter_done_first)
+	);
+filter_sos
+	 #(.COEF_SIZE(COEF_SIZE),
+ 	   .DATA_SIZE(DATA_SIZE),
+ 	  .B0(20'd262144),
+	  .B1(20'd528759),
+	  .B2(20'd262144),
+	  .A1(20'd531147),
+	  .A2(20'd259370),
+	  .GAIN(20'd260692))
+	sos_stage_2 (
+ 	 .data_in(data_connection_1),
+	 .reset(reset),
+	 .clk(clk),
+	 .data_out(data_out),
+	 .sample_trig(filter_done_first),
+	 .filter_done(filter_done_second)
+	);
+endmodule

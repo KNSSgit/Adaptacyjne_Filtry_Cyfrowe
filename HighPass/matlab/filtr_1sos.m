63c91bca1344f683e57978e7c5374be9fea8c03f
Adrian-Komputer\Adrian <aoleksiag@gmail.com>
2017-10-24 11:59:42 +0200

    dodano kom do fixpoint

--- HighPass/matlab/filtr_1sos.m
+++ HighPass/matlab/filtr_1sos.m
@@ -10,12 +10,11 @@ figure(1)
 
 for i=1:1
     for j=1:6
-        [A,sos2(i,j),sos3(i,j)]=fixpoint(sosl(i,j),liczba_bit);
+        sos3(i,j)=fixpoint(sosl(i,j),liczba_bit);
     end
 end
 
-bp(1,1:3)=sos2(1,1:3);
-ap(1,1:3)=sos2(1,4:6);
+
 
 probka=1000;
 R1=0;
@@ -23,28 +22,10 @@ R2=0;
 R3=0;
 k=1:200;
 
-sos2
+
 sos3
 
-[l,k,m]=fixpoint(g,liczba_bit);
+m=fixpoint(g,liczba_bit);
 
-hdl_creator(sos3,1,liczba_bit,m,'dmm')
-m
+hdl_creator(sos3,1,liczba_bit,m,'High_pas_top')
 
-for i=k
-       
-    b0_g=bp(1,1)*probka;
-    b1_g=bp(1,2)*probka;
-    b2_g=bp(1,3)*probka;
-    %R3p(i)=R3;
-    
-    R3=floor(b0_g+R1);
-    a1_g=R3*ap(1,2);
-    a2_g=R3*ap(1,3);
-    Rg=R3*g^(1/3);
-    R1=b1_g-a1_g+R2;
-    R2=b2_g-a2_g;
-end
-
-%figure(2)
-%plot(k,R3p)

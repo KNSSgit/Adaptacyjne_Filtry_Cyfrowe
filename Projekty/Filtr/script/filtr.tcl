4e751fc0189d96efde53c7f5be501ad76fec9360
aoleksiag <aoleksiag@gmail.com>
2017-10-24 17:17:10 +0200

    poprawiony skrypt do projekty filtr

--- Projekty/Filtr/script/filtr.tcl
+++ Projekty/Filtr/script/filtr.tcl
@@ -41,7 +41,7 @@
 #*****************************************************************************************
 
 # Set the reference directory for source file relative paths (by default the value is script directory path)
-set origin_dir "."
+set origin_dir "C:/KNSS_GIT/Adaptacyjne_Filtry_Cyfrowe/Projekty/Filtr/src"
 
 # Use origin directory path location variable, if specified in the tcl shell
 if { [info exists ::origin_dir_loc] } {

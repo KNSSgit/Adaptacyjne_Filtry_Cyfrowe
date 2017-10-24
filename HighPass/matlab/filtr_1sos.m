f72fc1aad6807dfcb4272b2f35fd4b63b975338c
Adrian-Komputer\Adrian <aoleksiag@gmail.com>
2017-10-24 12:08:33 +0200

    poprawki w kodach generujacych współczynniki

--- HighPass/matlab/filtr_1sos.m
+++ HighPass/matlab/filtr_1sos.m
@@ -3,6 +3,7 @@ close all
 fs=48000;
 fc=5000;
 liczba_bit=20;
+rzad=2;
 [b,a]=cheby2(2,30,2*fc/fs);
 [sosl,g] = tf2sos(b,a);
 figure(1)
@@ -15,17 +16,9 @@ for i=1:1
 end
 
 
-
-probka=1000;
-R1=0;
-R2=0;
-R3=0;
-k=1:200;
-
-
 sos3
 
 m=fixpoint(g,liczba_bit);
 
-hdl_creator(sos3,1,liczba_bit,m,'High_pas_top')
+hdl_creator(sos3,rzad/2,liczba_bit,m,'High_pas_top')
 

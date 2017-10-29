c85a50d6bec6eb21c161cb114740c18c997f21e9
Sebastian Cieślak <sebastiancieslak2@wp.pl>
2017-10-29 23:32:13 +0100

    drobna poprawka

--- Matlab/linear_prediction.m
+++ Matlab/linear_prediction.m
@@ -12,7 +12,7 @@
    sinus_zaburzony = sinus + temp;
    x = awgn(sinus, 5);              %dodanie szumu bialego do sinusa 50hz
 
-plot(1:160,sinus,'-r',1:160,sinus_zaburzony,'-g',1:160,x,'-b')
+plot(1:length(t),sinus,'-r',1:length(t),sinus_zaburzony,'-g',1:length(t),x,'-b')
 title 'Sygnaly przed filtracja'
 legend('50Hz','50Hz + 200Hz', '50Hz + Bialy szum')
 
@@ -26,7 +26,7 @@ est_b = filter([0 -b(2:end)],1,sinus_zaburzony);
 est_c = filter([0 -c(2:end)],1,x);
 
 figure
-plot(1:160,est_a,'-r',1:160,est_b,'-g',1:160,est_c,'-b')
+plot(1:length(t),est_a,'-r',1:length(t),est_b,'-g',1:length(t),est_c,'-b')
 title 'Sygnaly po filtracji'
 legend('50Hz','50Hz + 200Hz', '50Hz + Bialy szum')
 
@@ -35,6 +35,6 @@ f = sinus_zaburzony-est_b;
 g = x-est_c;
 
 figure
-plot(1:160,e,'-r',1:160,f,'-g',1:160,g,'-b')
+plot(1:length(t),e,'-r',1:length(t),f,'-g',1:length(t),g,'-b')
 title 'Roznica sygnalow przed i po filtracji'
 legend('50Hz','50Hz + 200Hz', '50Hz + Bialy szum')
